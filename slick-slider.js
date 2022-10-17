b07899de53082b92f2dee096a93c18ec71655fdc
Jonathan Britton <99915107+jbritttt@users.noreply.github.com>
2022-10-17 15:37:50 +0100

    Add files via upload

---
+++ slick-slider.js
@@ -0,0 +1,17 @@
+$('.slick-slider').slick({
+    autoplay: true,
+infinite: true,
+speed: 500,
+fade: true,
+cssEase: 'linear',
+prevArrow: ".prev",
+      nextArrow: ".next",
+});
+  
+
+$('.slick-slider-2').slick({
+    speed: 800,
+    prevArrow: ".slick-slider-2-prev-1",
+    nextArrow: ".slick-slider-2-next-1"
+
+});
