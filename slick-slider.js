e57b5a4e89942d85d4af957b598983f9db89530d
Jonathan Britton <99915107+jbritttt@users.noreply.github.com>
2022-10-24 00:32:40 +0100

    Update slick-slider.js

--- slick-slider.js
+++ slick-slider.js
@@ -1,4 +1,6 @@
-$('.slick-slider').slick({
+$(document).ready(function(){
+  
+  $('.slick-slider').slick({
     autoplay: true,
 infinite: true,
 speed: 500,
@@ -7,11 +9,14 @@ cssEase: 'linear',
 prevArrow: ".prev",
       nextArrow: ".next",
 });
-  
+});
 
+
+$(document).ready(function(){
 $('.slick-slider-2').slick({
     speed: 800,
     prevArrow: ".slick-slider-2-prev-1",
     nextArrow: ".slick-slider-2-next-1"
 
+});
 });
